b5fdc4a4ab0920a2b7695dabbfa9444458c74bcd
juginowakowska <>
2023-08-13 12:58:40 +0000

    Add category function added

--- recipebook/routes.py
+++ recipebook/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from recipebook import app, db
 from recipebook.models import Category, Recipe
 
@@ -6,4 +6,18 @@ from recipebook.models import Category, Recipe
 def home():
     return render_template("recipies.html")
 
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
 
