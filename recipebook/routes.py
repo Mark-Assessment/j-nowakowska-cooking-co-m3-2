f4253bd7996a95e360a59dd9deb21426026eda99
juginowakowska <>
2023-08-25 23:31:37 +0000

    css added

--- recipebook/routes.py
+++ recipebook/routes.py
@@ -85,8 +85,17 @@ def delete_recipe(recipe_id):
 
 
 
-'''
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    
-'''
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if "user" not in session:
+        if request.method == "POST":
+            existing_user = Login(username = request.form.get("username"))
+            if existing_user:
+                if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username")
+                    flash ("Welcome back,{}")
+        db.session.add(login)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("login.html")
